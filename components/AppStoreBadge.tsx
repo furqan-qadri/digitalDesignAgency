5305f605b86eb266dae9d6dd510f5c8ca7f7f2a0
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2023-10-28 20:44:44 +0800

    Revert "Revert "add stats""
    This reverts commit 388abe9ee490c4956733e6d9e74389d9bd1ab33b.

---
+++ components/AppStoreBadge.tsx
@@ -0,0 +1,12 @@
+// components/AppStoreBadge.tsx
+
+const AppStoreBadge: React.FC = () => {
+    return (
+      <a href="https://www.apple.com/app-store/">
+        <img src="./apple.svg" alt="Download on the App Store" />
+      </a>
+    );
+  };
+  
+  export default AppStoreBadge;
+  
---
+++ components/PlayStoreBadge.tsx
@@ -0,0 +1,12 @@
+// components/PlayStoreBadge.tsx
+
+const PlayStoreBadge: React.FC = () => {
+    return (
+      <a href="https://play.google.com/store">
+        <img src="/play-store-badge.png" alt="Get it on Google Play" />
+      </a>
+    );
+  };
+  
+  export default PlayStoreBadge;
+  
